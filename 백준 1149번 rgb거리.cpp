d445760d38dcf8387ed8e0cff345a154ed8b7184
saechimdaeki <40031858+saechimdaeki@users.noreply.github.com>
2019-01-26 22:14:16 +0900

    치명적 런타임을 해결 ㅋㅋ

--- 백준 1149번 rgb거리.cpp
+++ 백준 1149번 rgb거리.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-int dynamic[3][1000];
-
 int main()
 {
+	int dynamic[1001][3];
 	int num;
+	
 	
 	cin>>num;
 	for(int i=1; i<=num; i++)
@@ -16,6 +16,7 @@ int main()
 		dynamic[i][1]=min(dynamic[i-1][0],dynamic[i-1][2])+green;
 		dynamic[i][2]=min(dynamic[i-1][0],dynamic[i-1][1])+blue;
 	}	
-	cout<<min(dynamic[num][0],min(dynamic[num][1],dynamic[num][2]));
+	cout<<min(min(dynamic[num][0],dynamic[num][1]),dynamic[num][2]);
+
 		
 } 
