cd8747c01da29d7482b96f701f3104204e5b3876
saechimdaeki <40031858+saechimdaeki@users.noreply.github.com>
2019-01-26 19:14:11 +0900

    왠 오류가

--- 구구단.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-	int a;
-	cin>>a;
-	for(int i=1;i<=9;i++)
-	cout<<a<<" * "<<i<< " = "<<a*i<<endl;
-}
--- 백준 11654번 아스키코드.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	char a;
-	cin>>a;
-	cout<<a-'\0'<<endl;
-}
