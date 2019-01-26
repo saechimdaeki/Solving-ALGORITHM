6308dd947f11f2d23023d5bc52b6dd0365a474d4
saechimdaeki <40031858+saechimdaeki@users.noreply.github.com>
2019-01-26 19:15:03 +0900

    ㅇㅁ
    ㅇㅁ

--- 백준 1463번 1로 만들기.cpp
+++ 백준 1463번 1로 만들기.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int dynamic[1000000];
 
-int small(int a, int b)
+int min(int a, int b)
 {
 	if(a>b)
 	return b;
@@ -21,9 +21,9 @@ int small(int a, int b)
  	{
  		dynamic[i]=dynamic[i-1]+1;
  		if(i%2==0)
- 		dynamic[i]=small(dynamic[i],dynamic[i/2]+1);
+ 		dynamic[i]=min(dynamic[i],dynamic[i/2]+1);
  		if(i%3==0)
- 		dynamic[i]=small(dynamic[i],dynamic[i/3]+1);
+ 		dynamic[i]=min(dynamic[i],dynamic[i/3]+1);
 	 }
 	 cout<<dynamic[n]<<endl;
  	
