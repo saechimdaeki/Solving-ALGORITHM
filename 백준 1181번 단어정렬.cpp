6b2354ee0840f7828112d681beace90bd3ac0c08
saechimdaeki <40031858+saechimdaeki@users.noreply.github.com>
2019-02-08 16:16:57 +0900

    백준 1181번 단어정렬

---
+++ 백준 1181번 단어정렬.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+string abc[20000];
+bool cmp(string &s1, string &s2)
+{
+	if(s1.length()==s2.length())
+	return s1<s2;
+	return s1.length()<s2.length();
+}
+int main()
+{
+	int n;
+	cin>>n;
+	
+	for(int i=1; i<=n; i++)
+	cin>>abc[i];
+	sort(abc+1,abc+n+1,cmp);
+	for(int i=1; i<=n; i++)
+	if(abc[i]!=abc[i+1])
+	cout<<abc[i]<<endl;
+}
