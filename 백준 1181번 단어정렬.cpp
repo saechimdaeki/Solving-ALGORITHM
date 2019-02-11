3be1145fc57d7135a2dcc735876ae61195cfff77
saechimdaeki <40031858+saechimdaeki@users.noreply.github.com>
2019-02-11 11:43:42 +0900

    Update 백준 1181번 단어정렬.cpp

--- 백준 1181번 단어정렬.cpp
+++ 백준 1181번 단어정렬.cpp
@@ -2,7 +2,7 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-string abc[20000];
+string abc[20001];
 bool cmp(string &s1, string &s2)
 {
 	if(s1.length()==s2.length())
@@ -19,5 +19,5 @@ int main()
 	sort(abc+1,abc+n+1,cmp);
 	for(int i=1; i<=n; i++)
 	if(abc[i]!=abc[i+1])
-	cout<<abc[i]<<endl;
+	cout<<abc[i]<<"\n";
 }
