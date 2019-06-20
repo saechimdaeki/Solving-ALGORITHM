cdd97f8e14bd5484a14eabb09e42a0c6e7e26ca9
saechimdaeki <anima94@naver.com>
2019-06-20 15:43:23 +0900

    acos는 질문검색을 통해 알게되었음

---
+++ 백준 3053번택시 기하학.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+#define M_PI 3.14159265358979323846
+
+int r;
+const double pi =acos(-1.0);
+int main()
+{
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout<<fixed;
+	cout.precision(6);
+	cin>>r;
+	cout<<r*r*pi<<"\n";
+	cout<<r*r*2.0<<"\n";
+	
+}
