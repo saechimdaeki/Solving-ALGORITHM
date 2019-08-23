12d1fa665909e048405badc8f2e011d914fc04e0
junseong kim <40031858+saechimdaeki@users.noreply.github.com>
2019-08-23 13:02:06 +0900

    백준17254번 키보드 이벤트

---
+++ 백준 17254번 키보드 이벤트.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+struct keyboard{
+	int num,sec;
+	char k;
+};
+int n,m;
+int number,second;
+char ch;
+bool cmp(struct keyboard &a, struct keyboard &b){
+	if(a.sec==b.sec)
+	return a.num<b.num;
+	else
+	return a.sec<b.sec;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin>>n>>m;
+	vector<keyboard> v;
+	for(int i=0; i<m; i++)
+	{
+		cin>>number>>second>>ch;
+		v.push_back(make_pair{number,second,ch});
+	}
+	sort(v.begin(),v.end(),cmp);
+	for(int i=0; i<v.size(); i++)
+	cout<<v[i].k;
+}
