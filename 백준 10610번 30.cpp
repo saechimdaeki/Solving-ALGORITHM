64797c444a16679497966e9a433c23ac071e2890
junseong kim <anima94@kakao.com>
2020-04-07 16:48:20 +0900

    그리디

--- 백준 10610번 30.cpp
+++ 백준 10610번 30.cpp
@@ -1,24 +1,21 @@
 #include<iostream>
 #include<algorithm>
-#include<vector>
 using namespace std;
-
 string s;
-
+int answer,tmp;
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cin>>s;
-	int sum(0);
-	for(char c: s)
-	sum+=c-'0';
+	for(int i=0; i<s.length(); i++)
+	tmp+=(s[i]-'0');
 	sort(s.begin(),s.end());
-	if(s[0]=='0'&&sum%3==0)
+	if(tmp%3==0 && s[0]=='0')
 	{
 		reverse(s.begin(),s.end());
 		cout<<s;
-	}
-	else
-	cout<<"-1";
-} 
+	}else
+	cout<<-1;
+	
+}
