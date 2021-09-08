746884f0e11666a4ffee5fd2daa636f85391af4b
saechimdaeki <anima94@kakao.com>
2021-09-08 23:04:37 +0900

    2021- 09 -08 SameTree

---
+++ LeetCode-Kotlin&Java/LeetCode100/SameTree.kt
@@ -0,0 +1,26 @@
+package LeetCode100
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if(p==null || q==null) return p==q
+        return when (p.`val`) {
+            q.`val` -> isSameTree(p.left,q.left) && isSameTree(p.right,q.right)
+            else -> false
+        }
+    }
+}
