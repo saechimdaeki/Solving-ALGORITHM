ab4cb6ee2ab9a8966477700de27465a12c2fe663
Junseong kim <anima94@kakao.com>
2022-09-23 00:22:01 +0900

    2022-09-23 java8 연습용 풀이 LeetCode

---
+++ LeetCode-Kotlin&Java/LeetCode2103/Solution.java
@@ -0,0 +1,40 @@
+package LeetCode2103;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+/**
+ * LeetCode 2103. Rings and Rods
+ *     // input: B0R0G0R9R0B0G0 output: 1
+ *     // input: G4 output: 0
+ */
+class Solution {
+    public int countPoints(String rings) {
+        Map<Character, String> map = new HashMap<>();
+        IntStream.iterate(0, i -> i < rings.length(), i -> i + 2)
+                 .forEachOrdered(i -> {
+            char c1 = rings.charAt(i);
+            char c2 = rings.charAt(i + 1);
+            String s = map.computeIfAbsent(c2, character -> Character.toString(c1));
+            if (s.equals(String.valueOf(c1)))
+                return;
+            map.compute(c2, (character, string) -> map.get(c2) + c1);
+        });
+        AtomicInteger cnt= new AtomicInteger(0);
+        Predicate<String> predicate = (string) -> string.contains("R") &&  string.contains("G") && string.contains("B");
+        map.forEach((k,v) -> {
+            if(predicate.test(v))
+                cnt.getAndIncrement();
+        });
+        return cnt.get();
+    }
+
+}
+
+//    public static void main(String[] args) {
+//        System.out.println(        countPoints("B0R0G0R9R0B0G0"));
+//        System.out.println(        countPoints("G4"));
+//    }
