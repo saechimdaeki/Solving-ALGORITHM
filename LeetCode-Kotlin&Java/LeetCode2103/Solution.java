074e7f270d9889c8ee34f79e85883e742acca41f
Junseong kim <anima94@kakao.com>
2022-09-23 00:23:42 +0900

    2022-09-23 java8 연습용 풀이

--- LeetCode-Kotlin&Java/LeetCode2103/Solution.java
+++ LeetCode-Kotlin&Java/LeetCode2103/Solution.java
@@ -8,26 +8,25 @@ import java.util.stream.IntStream;
 
 /**
  * LeetCode 2103. Rings and Rods
- *     // input: B0R0G0R9R0B0G0 output: 1
- *     // input: G4 output: 0
+ * // input: B0R0G0R9R0B0G0 output: 1
+ * // input: G4 output: 0
  */
 class Solution {
     public int countPoints(String rings) {
         Map<Character, String> map = new HashMap<>();
         IntStream.iterate(0, i -> i < rings.length(), i -> i + 2)
                  .forEachOrdered(i -> {
-            char c1 = rings.charAt(i);
-            char c2 = rings.charAt(i + 1);
-            String s = map.computeIfAbsent(c2, character -> Character.toString(c1));
-            if (s.equals(String.valueOf(c1)))
-                return;
-            map.compute(c2, (character, string) -> map.get(c2) + c1);
-        });
-        AtomicInteger cnt= new AtomicInteger(0);
-        Predicate<String> predicate = (string) -> string.contains("R") &&  string.contains("G") && string.contains("B");
-        map.forEach((k,v) -> {
-            if(predicate.test(v))
+                     char c1 = rings.charAt(i);
+                     char c2 = rings.charAt(i + 1);
+                     map.compute(c2, (character, string) -> map.get(c2) + c1);
+                     map.computeIfAbsent(c2, character -> Character.toString(c1));
+                 });
+        AtomicInteger cnt = new AtomicInteger(0);
+        Predicate<String> predicate = (string) -> string.contains("R") && string.contains("G") && string.contains("B");
+        map.forEach((k, v) -> {
+            if (predicate.test(v)) {
                 cnt.getAndIncrement();
+            }
         });
         return cnt.get();
     }
