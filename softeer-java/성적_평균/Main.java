9bef934395cb61eaf7a4d2601d4fb8a96bf974ff
Junseong kim <anima94@kakao.com>
2024-11-24 16:50:33 +0900

    softeer lv3 성적 평균

--- softeer-java/성적_평균/Main.java
+++ softeer-java/성적_평균/Main.java
@@ -1,30 +1,28 @@
-package 자바.softeer.성적_평균;
+package 성적_평균;
 
-import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
-
+import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] nk = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int n = nk[0];
-        int k = nk[1];
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        double[] sum = new double[n+1];
-        IntStream.rangeClosed(1, n).forEachOrdered(i -> sum[i] = sum[i - 1] + arr[i - 1]);
-        while (k-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            double rangeSum = sum[end] - sum[start - 1];
-            double rangeAverage = rangeSum / (double) (end - start + 1);
-            bw.write(String.format("%.2f",rangeAverage)+"\n");
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] arr = new int[n];
+        int[] prefixSum = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+            prefixSum[i + 1] = prefixSum[i] + arr[i];
         }
-        bw.flush();
+        while (k-- > 0) {
+            int from = scanner.nextInt();
+            int to = scanner.nextInt();
+
+            int sum = prefixSum[to] - prefixSum[from - 1];
+
+            double avg = Math.round((sum * 1.0 / (to - from + 1)) * 100) / 100.00;
+            System.out.println(avg);
+        }
     }
 }
