ad93ca040d180913422086aa80586c9e40ad9f5f
Junseong kim <anima94@kakao.com>
2024-12-26 21:40:32 +0900

    softeer 택배 마스터 광우

--- softeer-java/택배_마스터_광우/Main.java
+++ softeer-java/택배_마스터_광우/Main.java
@@ -1,54 +1,60 @@
-package 자바.softeer.택배_마스터_광우;
+package softeer.택배_마스터_광우;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static int n;
-    static int m;
-    static int k;
-
+    static int n, m, k;
+    static int[] arr;
+    static boolean[] visited;
     static int answer = Integer.MAX_VALUE;
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] nmk = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        n = nmk[0];
-        m = nmk[1];
-        k = nmk[2];
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        boolean[] visited = new boolean[n];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
 
-        dfs(arr, 0, visited, new int[n]);
+        arr = new int[n];
+        visited = new boolean[n];
 
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        dfs(0, new int[n]);
         System.out.println(answer);
     }
 
-    static void dfs(int[] arr, int cnt, boolean[] visited, int[] temp) {
-        if (cnt == n) {
-            int idx = 0, bucket = 0, sum = 0;
-            for (int i = 0; i < k; i++) {
-                while (temp[idx] + bucket <= m) {
-                    bucket += temp[idx];
-                    idx = (idx + 1) % n;
-                }
-                sum += bucket;
-                bucket = 0;
-            }
-            answer = Math.min(answer, sum);
+    static void dfs(int count, int[] result) {
+        if (count == n) {
+            answer = Math.min(answer, calculate(result));
             return;
         }
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                temp[cnt] = arr[i];
-                dfs(arr, cnt + 1, visited, temp);
+                result[count] = arr[i];
+                dfs(count + 1, result);
                 visited[i] = false;
             }
         }
+    }
+
+    static int calculate(int[] result) {
+        int total = 0;
+        int index = 0;
+
+        for (int i = 0; i < k; i++) {
+            int weight = 0;
+            while (weight + result[index] <= m) {
+                weight += result[index];
+                if (index + 1 == n)
+                    index = 0;
+                else
+                    index++;
+            }
+            total += weight;
+        }
+
+        return total;
     }
 }
